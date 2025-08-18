1ac77a7e982d5a2d789f581c11fe752fcafc6c3e
i520e3e4 <i520e3e4@126.com>
2025-08-18 17:18:26 +0800

    Initial commit

--- js/config.js
+++ js/config.js
@@ -16,7 +16,7 @@ const SITE_CONFIG = {
     name: 'TeslaTV',
     url: 'https://teslatv.is-an.org',
     description: '免费在线视频搜索与观看平台',
-    logo: 'image/logo.png',
+    logo: 'image/logo.svg',
     version: '1.0.3'
 };
 
